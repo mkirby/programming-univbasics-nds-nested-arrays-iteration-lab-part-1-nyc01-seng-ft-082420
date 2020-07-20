8284b904a4e9237c9d1f0ad5def560a3ddaa8a77
Matthew Kirby <matthew.robbins.kirby@gmail.com>
2020-07-20 19:14:43 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -18,7 +18,7 @@ def find_even_values(src)
     inner_count = 0
     while inner_count < src[count].length do 
       
-      if src[count][inner_count] % 2 == 0
+      if src[count][inner_count].even?
         p src[count][inner_count]
       end
       inner_count += 1
